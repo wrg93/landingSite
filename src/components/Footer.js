2316ee4f58e827766799b86dfae3a5eefe4500d7
William Grundler <wrgrundler@gmail.com>
2020-06-24 11:06:28 -0700

    Footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <span>Grundler 2020</span>
+        </footer>
+    )
+}
+
+export default Footer;
