1d558a2f8482529ce6acd384bd0ebb892654cba2
William Grundler <wrgrundler@gmail.com>
2020-07-29 08:56:06 -0700

    resume link

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -1,7 +1,7 @@
   
 import React from "react";
 import "./Connect.css"
-
+import PDF from "../resume/WRGResume2020.pdf"
 
 function Connect(){
 
@@ -10,7 +10,7 @@ function Connect(){
         <span>
               <a href="https://www.linkedin.com/in/william-grundler-249154156/" id="linkedin" target="_blank" title="Linkedin Profile" rel="noopener noreferrer"><i className="fab fa-linkedin fa-3x"></i></a>
               <a href="https://github.com/wrg93" target="_blank" id="github" title="Github" rel="noopener noreferrer"><i className="fab fa-github-square fa-3x"></i></a>
-              <a href="https://docs.google.com/document/d/1pzqIrfJIrk2DM4tbkrLNwTQAh3ShzqMdUbEWi02uAJ8/edit" target="_blank" title="resume"rel="noopener noreferrer"><i className="fa fa-file fa-3x"></i></a>
+              <a href={PDF} target="_blank" title="resume"rel="noopener noreferrer"><i className="fa fa-file fa-3x"></i></a>
                 
         </span>
     )
