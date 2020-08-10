77219f9bd30ee4f7a4c2f84e3883ceece22b2ab9
William Grundler <wrgrundler@gmail.com>
2020-08-10 13:22:03 -0700

    fixed header and footer responsiveness

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -8,9 +8,9 @@ function Connect(){
     return(
 
         <span>
-              <a href="https://www.linkedin.com/in/william-grundler-249154156/" id="linkedin" target="_blank" title="Linkedin Profile" rel="noopener noreferrer"><i className="fab fa-linkedin fa-3x"></i></a>
-              <a href="https://github.com/wrg93" target="_blank" id="github" title="Github" rel="noopener noreferrer"><i className="fab fa-github-square fa-3x"></i></a>
-              <a href={PDF} target="_blank" title="resume"rel="noopener noreferrer"><i className="fa fa-file fa-3x"></i></a>
+              <a href="https://www.linkedin.com/in/william-grundler-249154156/" id="linkedin" target="_blank" title="Linkedin Profile" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+              <a href="https://github.com/wrg93" target="_blank" id="github" title="Github" rel="noopener noreferrer"><i className="fab fa-github-square fa-2x"></i></a>
+              <a href={PDF} target="_blank" title="resume"rel="noopener noreferrer"><i className="fa fa-file fa-2x"></i></a>
                 
         </span>
     )
